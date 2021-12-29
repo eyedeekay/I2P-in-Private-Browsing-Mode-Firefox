c58cf630270bb1bb3af86443e923cac1dc3b966b
idk <hankhill19580@gmail.com>
2021-12-28 20:36:17 -0500

    merge

--- content.js
+++ content.js
@@ -2,54 +2,54 @@ function contentUpdateById(id, message) {
   let infoTitle = document.getElementById(id);
   let messageContent = chrome.i18n.getMessage(message);
   if (infoTitle === null) {
-    console.log("content error", id, messageContent);
+    console.log('content error', id, messageContent);
     return;
   }
   infoTitle.textContent = messageContent;
 }
 
 // Information Section
-contentUpdateById("text-section-header", "extensionName");
-contentUpdateById("description", "extensionDescription");
-contentUpdateById("i2pbrowser-version", "extensionVersion");
+contentUpdateById('text-section-header', 'extensionName');
+contentUpdateById('description', 'extensionDescription');
+contentUpdateById('i2pbrowser-version', 'extensionVersion');
 
 // Control Section
-contentUpdateById("controlHeader", "controlHeader");
-contentUpdateById("controlExplain", "controlExplain");
-contentUpdateById("clear-browser-data", "clearData");
-contentUpdateById("clear-desc", "clearDesc");
-contentUpdateById("enable-web-rtc", "enableWebRTC");
-contentUpdateById("rtcDesc", "rtcDesc");
-contentUpdateById("disable-history", "disableHistory");
-contentUpdateById("histDesc", "histDesc");
+contentUpdateById('controlHeader', 'controlHeader');
+contentUpdateById('controlExplain', 'controlExplain');
+contentUpdateById('clear-browser-data', 'clearData');
+contentUpdateById('clear-desc', 'clearDesc');
+contentUpdateById('enable-web-rtc', 'enableWebRTC');
+contentUpdateById('rtcDesc', 'rtcDesc');
+contentUpdateById('disable-history', 'disableHistory');
+contentUpdateById('histDesc', 'histDesc');
 
 // Application Section
-contentUpdateById("applicationHeader", "applicationHeader");
-contentUpdateById("applicationExplain", "applicationExplain");
-contentUpdateById("window-visit-index", "windowVisitHelppage");
-contentUpdateById("help", "help");
-contentUpdateById("window-visit-router", "windowVisitConsole");
-contentUpdateById("routerConsole", "routerConsole");
-contentUpdateById("window-visit-homepage", "windowVisitHomepage");
-contentUpdateById("abouthome", "abouthome");
-contentUpdateById("window-visit-i2ptunnel", "windowVisitI2ptunnel");
-contentUpdateById("i2ptunnel", "i2ptunnel");
-contentUpdateById("window-visit-susimail", "windowVisitSusiMail");
-contentUpdateById("susimail", "susimail");
-contentUpdateById("window-visit-snark", "windowVisitSnark");
-contentUpdateById("snark", "snark");
+contentUpdateById('applicationHeader', 'applicationHeader');
+contentUpdateById('applicationExplain', 'applicationExplain');
+contentUpdateById('window-visit-index', 'windowVisitHelppage');
+contentUpdateById('help', 'help');
+contentUpdateById('window-visit-router', 'windowVisitConsole');
+contentUpdateById('routerConsole', 'routerConsole');
+contentUpdateById('window-visit-homepage', 'windowVisitHomepage');
+contentUpdateById('abouthome', 'abouthome');
+contentUpdateById('window-visit-i2ptunnel', 'windowVisitI2ptunnel');
+contentUpdateById('i2ptunnel', 'i2ptunnel');
+contentUpdateById('window-visit-susimail', 'windowVisitSusiMail');
+contentUpdateById('susimail', 'susimail');
+contentUpdateById('window-visit-snark', 'windowVisitSnark');
+contentUpdateById('snark', 'snark');
 
 // Homepage Section
-contentUpdateById("window-visit-webpage", "windowVisitWebPage");
-contentUpdateById("webpage", "help");
-contentUpdateById("window-visit-sources", "windowVisitSources");
-contentUpdateById("sources", "sources");
-contentUpdateById("window-visit-releases", "windowVisitReleases");
-contentUpdateById("releases", "releases");
+contentUpdateById('window-visit-webpage', 'windowVisitWebPage');
+contentUpdateById('webpage', 'help');
+contentUpdateById('window-visit-sources', 'windowVisitSources');
+contentUpdateById('sources', 'sources');
+contentUpdateById('window-visit-releases', 'windowVisitReleases');
+contentUpdateById('releases', 'releases');
 
-fetch("http://proxy.i2p").then((myJson) => {
-  contentUpdateById("proxy-check", "proxySuccessStatus");
-  let readyness = document.querySelectorAll(".readyness");
+fetch('http://proxy.i2p').then((myJson) => {
+  contentUpdateById('proxy-check', 'proxySuccessStatus');
+  let readyness = document.querySelectorAll('.readyness');
   if (readyness != null) {
     hide(readyness);
   }
