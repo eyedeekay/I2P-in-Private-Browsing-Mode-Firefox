d6402191fd69e5ebfd61aa9eca0ccfa84b7abddc
idk <hankhill19580@gmail.com>
2022-10-03 20:29:14 -0400

    Update images

---
+++ icons/download.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env sh
+rm -fv browse.png i2plogo.png torrents.png
+wget -c -O browse.png https://user-images.githubusercontent.com/50714166/193657979-44a7d972-ef4a-4f29-90aa-ac5c8f06a117.png
+wget -c -O i2plogo.png https://user-images.githubusercontent.com/50714166/193657981-14dd87a6-5c1b-4dc5-99ca-10afdfdd5dff.png
+wget -c -O torrents.png https://user-images.githubusercontent.com/50714166/193657982-0f0a604f-a360-482f-b918-75468f27fabe.png
+
+rm -fv infotoopie.png infotoopiebt.png infotoopies.png infotoopiesbt.png
+wget -c -O infotoopie.png https://user-images.githubusercontent.com/50714166/193660202-3069b61b-26fe-4120-8411-2985661656c2.png
+wget -c -O infotoopiebt.png https://user-images.githubusercontent.com/50714166/193660200-ea8d1318-03ea-4df4-aa8f-83d24dc74071.png
+wget -c -O infotoopies.png https://user-images.githubusercontent.com/50714166/193660199-d4c9d66b-d877-44cc-848e-aa31d152fa57.png
+wget -c -O infotoopiesbt.png https://user-images.githubusercontent.com/50714166/193660200-ea8d1318-03ea-4df4-aa8f-83d24dc74071.png
