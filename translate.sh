b2e3e7fcf06e34114af35f14192895d0b7349f89
idk <hankhill19580@gmail.com>
2022-10-07 19:30:57 -0400

    update translation generator script

--- translate.sh
+++ translate.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/env sh
 webext-translator -lang="ar"
-git commit -am "checkin translations" && git push ---all
+git commit -am "checkin translations ar" && git push ---all
 webext-translator -lang="zh"
-git commit -am "checkin translations" && git push ---all
+git commit -am "checkin translations zh" && git push ---all
 webext-translator -lang="fr"
-git commit -am "checkin translations" && git push ---all
+git commit -am "checkin translations fr" && git push ---all
 webext-translator -lang="de"
-git commit -am "checkin translations" && git push ---all
+git commit -am "checkin translations de" && git push ---all
 webext-translator -lang="it"
-git commit -am "checkin translations" && git push ---all
+git commit -am "checkin translations it" && git push ---all
 webext-translator -lang="ja"
-git commit -am "checkin translations" && git push ---all
+git commit -am "checkin translations ja" && git push ---all
 webext-translator -lang="pt"
-git commit -am "checkin translations" && git push ---all
+git commit -am "checkin translations pt" && git push ---all
 webext-translator -lang="ru"
-git commit -am "checkin translations" && git push ---all
+git commit -am "checkin translations ru" && git push ---all
 webext-translator -lang="es"
-git commit -am "checkin translations" && git push ---all
+git commit -am "checkin translations ex" && git push ---all
