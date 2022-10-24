d1aa9937b2f481d2aeeefabd7747c50ae426199f
idk <hankhill19580@gmail.com>
2022-10-24 18:49:57 -0400

    add beginnings of a CSP to manifestv3

--- Makefile
+++ Makefile
@@ -37,8 +37,8 @@ clean: rc clean-artifacts
 ## EVEN RELEASES are AMO RELEASES
 ## ODD RELEASES are SELFHOSTED RELEASES
 
-MOZ_VERSION=1.40
-VERSION=1.39
+MOZ_VERSION=1.42
+VERSION=1.41
 
 ## INCREMENT THIS EVERY TIME YOU DO A RELEASE
 LAST_VERSION=$(shell grep '"version"' manifest.json | sed 's|"version"||g' | tr -d " :,'" | tr -d '"')
---
+++ consoleinfo.js
@@ -0,0 +1,65 @@
+document.addEventListener(
+    'DOMContentLoaded',
+    function() {
+        fetch('http://127.0.0.1:7657').then(
+            (myJson) => {
+                console.warn('(consoleinfo)', myJson);
+                contentUpdateById('router-check', 'consoleSuccessStatus');
+                let routerness = document.querySelectorAll('.routerness');
+                if (routerness !== null) {
+                    unhide(routerness);
+                }
+            },
+            (error) => {
+                console.error('(consoleinfo)', error);
+                contentUpdateById('router-check', 'consoleFailedStatus');
+                let routerness = document.querySelectorAll('.routerness');
+                if (routerness !== null) {
+                    hide(routerness);
+                }
+            }
+        );
+    },
+    false
+);
+
+function hide(elements) {
+    elements = elements.length ? elements : [elements];
+    for (var index = 0; index < elements.length; index++) {
+        if (elements[index].style !== undefined) {
+            elements[index].style.display = "none";
+        }
+    }
+}
+
+function unhide(elements) {
+    elements = elements.length ? elements : [elements];
+    for (var index = 0; index < elements.length; index++) {
+        if (elements[index].style !== undefined) {
+            elements[index].style.display = "inline-block";
+        }
+    }
+}
+
+//TODO: Don't hard-code this.
+/*
+fetch("http://127.0.0.1:7657/themes/console/light/images/i2plogo.png")
+  .then((myJson) => {
+    var consoleLinks = document.querySelectorAll(".application-info");
+    unhide(consoleLinks);
+  })
+  .catch((error) => {
+    var consoleLinks = document.querySelectorAll(".application-info");
+    hide(consoleLinks);
+  });
+
+fetch("http://127.0.0.1:7657/jsonrpc/")
+  .then((myJson) => {
+    var toopieLinks = document.querySelectorAll(".window-visit-toopie");
+    unhide(toopieLinks);
+  })
+  .catch((error) => {
+    var toopieLinks = document.querySelectorAll(".window-visit-toopie");
+    hide(toopieLinks);
+  });
+*/
